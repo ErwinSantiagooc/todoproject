0fab1a014fccd9866501d41557243c98c1a0f5eb
Titsu-Maki <cristianalejandrovecr@ufps.edu.co>
2024-09-27 12:07:40 -0500

    feat(models): add Product model

--- products/models.py
+++ products/models.py
@@ -13,3 +13,18 @@ class ProductType(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+class Product(models.Model):
+    STATUSES = [
+        ("active", "Active"),
+        ("inactive", "Inactive")
+                
+       ]
+    name=models.CharField(max_length=100)
+    description=models.TextField(null=True, blank=True)
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    product_type=models.ForeignKey(ProductType, on_delete=models.CASCADE, related_name='products')
+    status = models.CharField(max_length=55, choices=STATUSES, default="active")
+    
+    def __str__(self) -> str:
+        return self.name
